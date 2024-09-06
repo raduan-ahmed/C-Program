a4837e65890bfa03e78fabf8fae588d81cafb42b
raduan-ahmed <raduanahmed49591@gmail.com>
2024-09-06 23:15:21 +0600

    commit-4

---
+++ 2-C_Fundamental/6-If-----Else/1-if.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int x = 20;
+  int y = 18;
+  if (x > y) {
+    printf("x is greater than y");  
+  }
+  printf("\n\n");
+  
+  if (20 > 18) {
+    printf("20 is greater than 18");
+  }  
+  return 0;
+}
